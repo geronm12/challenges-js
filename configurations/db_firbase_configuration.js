3fba1266e62dd8f45eca326d111b351df436beb4
Gerónimo <gerolpz01@gmail.com>
2023-01-05 02:21:13 -0300

    creados servicios básicos para agregar y leer documentos con firestore

---
+++ configurations/db_firbase_configuration.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCAHJZDDTJms0cYqfs226UHqAq6RwdKmBo",
+  authDomain: "js-challenges-24ad6.firebaseapp.com",
+  projectId: "js-challenges-24ad6",
+  storageBucket: "js-challenges-24ad6.appspot.com",
+  messagingSenderId: "32361193929",
+  appId: "1:32361193929:web:72ed49b4eb8d519e9a8818",
+  measurementId: "G-6LX40HB2NH",
+};
---
+++ services/db_service.js
@@ -0,0 +1,30 @@
+const {
+  firebaseConfig,
+} = require("../configurations/db_firbase_configuration");
+
+const { initializeApp } = require("firebase/app");
+const {
+  getFirestore,
+  addDoc,
+  collection,
+  where,
+  query,
+  getDocs,
+} = require("firebase/firestore");
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+const add = async (doc, docName) => {
+  try {
+    const docRef = await addDoc(collection(db, docName), doc);
+    console.log("Document written with ID: ", docRef.id);
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+};
+
+const readAll = async (docName) => await getDocs(collection(db, docName));
+
+const read = async (docName, ...args) =>
+  await getDocs(query(collection(db, docName), ...args));
